f470cc4e6cf6dd5b5dd0d887d7b2ac94f5aed00f
Avery Kemp <afkemp@unc.edu>
2025-09-20 17:04:59 -0400

    Add files via upload

---
+++ P1LAB1_AveryKemp.py
@@ -0,0 +1,12 @@
+# Avery Kemp
+# 09/20/2025
+# P1LAB1
+# This program asks the user for their first and last name
+# and then displays a welcome message.
+
+# Get user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display output
+print("Welcome to this course,", first_name, last_name + "!")
